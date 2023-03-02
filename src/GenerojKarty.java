7c0d1ab9affa262a4bfcb3324257c69ddc7bac24
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2023-03-02 14:57:49 +0100

    Initial commit

---
+++ src/GenerojKarty.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GenerojKarty {
+
+    public static ArrayList<Karta> generujKarty() {
+
+        //Tworzę pierwszą kartę
+        ArrayList<Karta> karty = new ArrayList<>();
+        int wartosc = ThreadLocalRandom.current().nextInt(1, 13);
+        int kolor = ThreadLocalRandom.current().nextInt(0, 3);
+        karty.add(new Karta(wartosc, kolor));
+
+    while(true) {
+        wartosc = ThreadLocalRandom.current().nextInt(0, 13);
+        if (wartosc == 0) {break;}
+        kolor = ThreadLocalRandom.current().nextInt(0, 3);
+        ListIterator<Karta> iterator  = karty.listIterator();
+
+        while(true) { // dodawanie iteratorem
+            if (!iterator.hasNext()) {iterator.add(new Karta(wartosc, kolor)); break;} // Jeżeli jestem na końcu listy to dodaje tutaj.
+            Karta nastepna = iterator.next();
+            if (nastepna.getWartosc() < wartosc) {iterator.add(new Karta(wartosc,kolor)); break;}
+            if (nastepna.getWartosc() == wartosc && nastepna.getKolor() > kolor) {iterator.next(); continue;}
+            if (nastepna.getWartosc() == wartosc && nastepna.getKolor() <= kolor) {iterator.next(); iterator.add(new Karta(wartosc, kolor)); break;}
+            if (nastepna.getWartosc() > wartosc) {iterator.next();continue;}
+        }
+
+    }
+
+        return karty;
+    }
+}
---
+++ src/Karta.java
@@ -0,0 +1,18 @@
+public class Karta {
+
+    protected int Wartosc;
+    protected int Kolor;
+
+    public Karta(int Wartosc, int Kolor) {
+        this.Wartosc = Wartosc;
+        this.Kolor = Kolor;
+    }
+
+    public int getWartosc() {
+        return Wartosc;
+    }
+
+    public int getKolor() {
+        return Kolor;
+    }
+}
